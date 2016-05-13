8c8851f771534d2f0399bf17910ca8617b90a6dd
Noelkd <nkdoherty@gmail.com>
2016-05-13 11:28:02 +0100

    Update death message

--- messages.py
+++ messages.py
@@ -3,7 +3,7 @@ death = [
     "Not like this... not like this...",
     "I regret nothing!",
     "*Dives out window*",
-    "It had to end some time, it had to end where",
+    "It had to end some time, it had to end some where",
     "I regret everything!",
     "By the Gods what fresh hell is this",
     "Where is that light I'm supposed to walk into?",
